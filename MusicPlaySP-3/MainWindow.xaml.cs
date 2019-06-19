7ba77249e563c190b38ab8f9ab817d1bdd04ff7f
Nauryzbai <nauzik@mail.ru>
2019-06-19 16:55:11 +0600

    ThreadSound

---
+++ MusicPlaySP-3/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MusicPlaySP_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var thread = new Thread(PlaingMusic);
+            thread.IsBackground = true;
+            thread.Start();
+            PlaingMusic();
+        }
+
+        public WMPLib.WindowsMediaPlayer WMP = new WMPLib.WindowsMediaPlayer();
+
+        public void PlaingMusic()
+        {
+            WMP.URL = @"Linkin_Park_-_New_Divide.mp3";
+            WMP.controls.play();
+            //WMP.close();
+        }
+
+        private void IfWindowsClose(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Thread myThread = new Thread(SaveRichBox);
+            myThread.Start();
+        }
+
+        private void SaveRichBox()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("SavingText.txt", true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(new TextRange(richTextBox.Document.ContentStart, richTextBox.Document.ContentEnd).Text);
+                    sw.Close();
+                }   
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+    }
+}
